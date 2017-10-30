e7ac0adccde229ee3c02e3b4ddfb8a327c1011b7
Toshiyuki Masui <masui@pitecan.com>
2017-10-30 18:48:11 +0900

    変数に漢字利用

--- public/javascripts/mv.js
+++ public/javascripts/mv.js
@@ -14,33 +14,29 @@ var button31 = { left:  20, top:  50, width: 250, height:  90 };
 var button32 = { left: 275, top:  50, width: 250, height:  90 };
 var button33 = { left: 530, top:  50, width: 250, height:  90 };
 
-var states = [
-    {
-	name: 'トップ',
-	image: 'https://gyazo.com/2069fefaec99bff27e6fde58f90bcd7e.png',
-	buttons: [
-	    [button11, '指定席選択'],
-	    [button12, '指定席選択', function(){ alert('指定席選択'); }]
-	]
-    },
-    {
-	name: '指定席選択',
-	image: 'https://gyazo.com/3a1f7bd4053a1989b9d32e1f2b8ce30e.png',
-	buttons: [
-	    [button21, '新幹線指定席選択'],
-	    [button22, 'トップ', function(){ alert('トップに戻る'); }]
-	]
-    },
-    {
-	name: '新幹線指定席選択',
-	image: 'https://gyazo.com/b6a4379be160f099e8ce568a03f67793.png',
-	buttons: [
-	    [button31, 'トップ'],
-	    [button32, 'トップ'],
-	    [button33, 'トップ', function(){ alert('トップに戻る'); }]
-	]
-    }
-];
+var トップ, 指定席選択, 新幹線指定席選択;
+トップ = {
+    image: 'https://gyazo.com/2069fefaec99bff27e6fde58f90bcd7e.png',
+    buttons: [
+	[button11, 指定席選択],
+	[button12, 指定席選択, function(){ alert('指定席選択'); }]
+    ]
+};
+指定席選択 = {
+    image: 'https://gyazo.com/3a1f7bd4053a1989b9d32e1f2b8ce30e.png',
+    buttons: [
+	[button21, 新幹線指定席選択],
+	[button22, トップ, function(){ alert('トップに戻る'); }]
+    ]
+};
+新幹線指定席選択 = {
+    image: 'https://gyazo.com/b6a4379be160f099e8ce568a03f67793.png',
+    buttons: [
+	[button31, トップ],
+	[button32, トップ],
+	[button33, トップ, function(){ alert('トップに戻る'); }]
+    ]
+};
 
 function transfunc(s, f){
     return function(){
@@ -49,8 +45,26 @@ function transfunc(s, f){
     };
 }
 
-function trans(s){ // sに遷移
+function trans(state){ // stateに遷移
     $('body').empty();
+    var image = $('<img>');
+    image.attr('src',state.image);
+    image.css('width',800);
+    $('body').append(image);
+    for(var j=0;j<state.buttons.length;j++){
+	var button = state.buttons[j][0];
+	var div = $('<div>').
+		css('position','absolute').
+		css('background-color','#ff0').
+		css('top',button.top).
+		css('left',button.left).
+		css('width',button.width).
+		css('height',button.height).
+		css('opacity',0.5);
+	div.on('click',transfunc(state.buttons[j][1], state.buttons[j][2]));
+	$('body').append(div);
+    }
+    /*
     for(var i=0;i<states.length;i++){
 	if(states[i].name == s){
 	    var state = states[i];
@@ -74,6 +88,7 @@ function trans(s){ // sに遷移
 	    break;
 	}
     }
+     */
 };
 
 $(function() {
