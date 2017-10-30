482854f9958531430807bf9216c359452cf0bf2f
Toshiyuki Masui <masui@pitecan.com>
2017-10-30 12:12:15 +0900

    cross_origin追加

--- connectouch.rb
+++ connectouch.rb
@@ -19,17 +19,20 @@ connection = Mongo::Connection.new('localhost', 27017)
 db = connection.db('connectouch')
 
 get '/read/:id' do |id|
+  cross_origin
   data = db['node'].find_one(:id => id)
   data.to_json # can be "null"
 end
 
 get '/read' do
+  cross_origin
   id = params[:id]
   data = db['node'].find_one(:id => id)
   data.to_json
 end
 
 get '/write/:id' do |id| # /write/abc?url=xyz, etc.
+  cross_origin
   data = params.clone
   data.delete('splat')
   data.delete('captures')
@@ -41,6 +44,7 @@ get '/write/:id' do |id| # /write/abc?url=xyz, etc.
 end
 
 get '/write' do # /write/abc?id=abc&url=xyz, etc.
+  cross_origin
   data = params.clone
   data.delete('splat')
   data.delete('captures')
@@ -52,10 +56,12 @@ get '/write' do # /write/abc?id=abc&url=xyz, etc.
 end
 
 get '/nodes' do
+  cross_origin
   db['node'].find().to_a.to_json
 end
 
 get '/addlink/:id1/:id2' do |id1,id2|
+  cross_origin
   data = {}
   data['time'] = Time.now.to_i
   data['url'] = params[:url]
@@ -65,6 +71,7 @@ get '/addlink/:id1/:id2' do |id1,id2|
 end
 
 get '/addlink' do
+  cross_origin
   id1 = params['id1']
   id2 = params['id2']
   data = {}
@@ -78,12 +85,14 @@ get '/addlink' do
 end
 
 get '/removelink/:id1/:id2' do |id1,id2|
+  cross_origin
   db['link'].remove(:link => [id1, id2])
   db['link'].remove(:link => [id2, id1])
   'true'
 end
 
 get '/removelink' do
+  cross_origin
   id1 = params['id1']
   id2 = params['id2']
   if id1 && id2 then
@@ -94,6 +103,7 @@ get '/removelink' do
 end
 
 get '/links' do
+  cross_origin
   db['link'].find().to_a.to_json
 end
 
