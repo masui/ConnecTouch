525f6ea8b2701d2eeba601b8dd017d9dc46645c5
Toshiyuki Masui <masui@pitecan.com>
2017-11-04 07:33:56 +0900

    変なサイトを登録しないようにする

--- RememberURL/rememberurl.js
+++ RememberURL/rememberurl.js
@@ -4,13 +4,24 @@
 // 現在のマシンからどのURLを見てるかを通知
 //
 $(function(){
-    //$.ajax('http://localhost/~masui/rememberurl.cgi', // ローカル
-    $.ajax('https://www.pitecan.com/RememberURL/remember.cgi', // pitecan.com では駄目
-	   {
-	       type: 'get',
-	       // async: false,
-	       data: { url: location.href, title: document.title, id: 'masui' },
-	       dataType: 'text'
-	   }
-	  );
+    var valid = true;
+    var ignorePatterns = [
+	/wifemovie/
+    ];
+    for(var pattern in ignorePatterns){
+	if(location.href.match(pattern)){
+	    valid = false;
+	}
+    }
+    if(valid){
+	$.ajax('https://www.pitecan.com/RememberURL/remember.cgi', // pitecan.com では駄目
+	       {
+		   type: 'get',
+		   // async: false,
+		   data: { url: location.href, title: document.title, id: 'masui' },
+		   dataType: 'text'
+	       }
+	      );
+    }
 });
+  
