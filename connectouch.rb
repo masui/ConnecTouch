cfef4ce4aa578f86f15d7463a447a0eaad35a838
Toshiyuki Masui <masui@pitecan.com>
2018-11-21 10:13:55 +0900

    Mongo gemの仕様変更(?)のため書き換え

--- connectouch.rb
+++ connectouch.rb
@@ -9,69 +9,70 @@
 # > db.link.find()
 
 require 'sinatra'
-require 'sinatra/cross_origin'
+# require 'sinatra/cross_origin'
 require 'mongo'
 require 'json'
 
-enable :cross_origin
+# enable :cross_origin
 
-connection = Mongo::Connection.new('localhost', 27017)
-db = connection.db('connectouch')
+# いろいろMongoの仕様が変わった?
+# connection = Mongo::Connection.new('localhost', 27017)
+# db = connection.db('connectouch')
+
+client = Mongo::Client.new('mongodb://localhost:27017/connectouch')
+db = client.database
 
 get '/read/:id' do |id|
-  # cross_origin
-  data = db['node'].find_one(:id => id)
-  data.to_json # can be "null"
+  data = db['node'].find(:id => id)
+  data.collect { |document|
+    document
+  }.to_json
 end
 
 get '/read' do
-  # cross_origin
   id = params[:id]
-  data = db['node'].find_one(:id => id)
-  data.to_json
+  data = db['node'].find(:id => id)
+  data.collect { |document|
+    document
+  }.to_json
 end
 
 get '/write/:id' do |id| # /write/abc?url=xyz, etc.
-  # cross_origin
   data = params.clone
   data.delete('splat')
   data.delete('captures')
   if data != {} then
-    db['node'].remove(:id => id) # remove all items with id
-    db['node'].insert(data)
+    db['node'].delete_many(:id => id) # remove all items with id
+    db['node'].insert_one(data)
   end
   data.to_json
 end
 
-get '/write' do # /write/abc?id=abc&url=xyz, etc.
-  # cross_origin
+get '/write' do # /write?id=abc&url=xyz, etc.
   data = params.clone
   data.delete('splat')
   data.delete('captures')
   if data['id'] then
-    db['node'].remove(:id => data['id']) # remove all items with id
-    db['node'].insert(data)
+    db['node'].delete_many(:id => data['id']) # remove all items with id
+    db['node'].insert_one(data)
   end
   data.to_json
 end
 
 get '/nodes' do
-  # cross_origin
   db['node'].find().to_a.to_json
 end
 
 get '/addlink/:id1/:id2' do |id1,id2|
-  # cross_origin
   data = {}
   data['time'] = Time.now.to_i
   data['url'] = params[:url]
   data['link'] = [id1, id2]
-  db['link'].insert(data)
+  db['link'].insert_one(data)
   data.to_json
 end
 
 get '/addlink' do
-  # cross_origin
   id1 = params['id1']
   id2 = params['id2']
   data = {}
@@ -79,25 +80,23 @@ get '/addlink' do
     data['time'] = Time.now.to_i
     data['url'] = params[:url]
     data['link'] = [id1, id2]
-    db['link'].insert(data)
+    db['link'].insert_one(data)
   end
   data.to_json
 end
 
 get '/removelink/:id1/:id2' do |id1,id2|
-  # cross_origin
-  db['link'].remove(:link => [id1, id2])
-  db['link'].remove(:link => [id2, id1])
+  db['link'].remove_many(:link => [id1, id2])
+  db['link'].remove_many(:link => [id2, id1])
   'true'
 end
 
 get '/removelink' do
-  # cross_origin
   id1 = params['id1']
   id2 = params['id2']
   if id1 && id2 then
-    db['link'].remove(:ids => [id1, id2])
-    db['link'].remove(:ids => [id2, id1])
+    db['link'].remove_many(:ids => [id1, id2])
+    db['link'].remove_many(:ids => [id2, id1])
   end
   'true'
 end
