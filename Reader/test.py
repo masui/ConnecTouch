54df161740cf9a7925770555bbd8beea8baaa7f4
Takumi Hayakawa <Hykwtakumin@users.noreply.github.com>
2018-11-21 23:24:59 +0900

    プログラムのロード時ではなくイベント発生時にIPアドレスを取得する方式に変更

--- Reader/test.py
+++ Reader/test.py
@@ -9,14 +9,14 @@ from datetime import datetime
 from uuid import getnode as get_mac
 import commands
 
-readerId = commands.getoutput("ip a show wlan0 | grep 'inet ' | cut -f6 -d ' '") # リーダに接続されたマシンのIPアドレス
-
 def startup(targets):
     print 'waiting for NFC tag ...'
     return targets
 
 def connected(tag):
+    readerId = commands.getoutput("ip a show wlan0 | grep 'inet ' | cut -f6 -d ' '") # リーダに接続されたマシンのIPアドレス
     nfcId = binascii.hexlify(tag.identifier)
+    
     date = datetime.now().strftime("%Y/%m/%d %H:%M:%S")
     print("|%s| readerId: %s nfcId: %s" % (date, readerId, nfcId))
     return id
