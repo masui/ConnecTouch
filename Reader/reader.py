5509c63651dec0231774570ee96ec2eb1f0cf90d
Takumi Hayakawa <Hykwtakumin@users.noreply.github.com>
2018-11-21 23:26:28 +0900

    IPアドレスを実行時かつタッチイベント発生時に取得するよう変更

--- Reader/reader.py
+++ Reader/reader.py
@@ -14,8 +14,6 @@ from subprocess import Popen
 import os
 import commands
 
-readerId = commands.getoutput("ip a show wlan0 | grep 'inet ' | cut -f6 -d ' '") # リーダに接続されたマシンのIPアドレス
-
 def lasturl():
     LOGFILE = '/Users/masui/Sites/rememberurl.log'
     loglines = open(LOGFILE).read().split('\n')
@@ -23,22 +21,35 @@ def lasturl():
     [time, url] = lastline.split('\t')
     return url
 
+def getIpAddrFromConf():
+    conf = open('/etc/dhcpcd.conf')
+    lines = conf.readlines()
+    conf.close()
+
+    for i in range(len(lines)):
+        if i == 41:
+            line = lines[i]
+            return line[-14:-1]
+
+readerId = getIpAddrFromConf()
+
 def startup(targets):
     print 'waiting for NFC tag ...'
     return targets
 
 def connected(tag):
+    readerIdFromIp = commands.getoutput("ip a show wlan0 | grep 'inet ' | cut -f6 -d ' ' | cut -c1-13") # リーダに接続されたマシンのIPアドレス
     nfcId = binascii.hexlify(tag.identifier)
     date = datetime.now().strftime("%Y/%m/%d %H:%M:%S")
-    print("|%s| readerId: %s nfcId: %s" % (date, readerId, nfcId))
+    print("|%s| readerId: %s nfcId: %s" % (date, readerIdFromIp, nfcId))
     
     Popen('/home/pi/ConnecTouch/Reader/led.sh',shell=True)
 
     #if readerId == "a45e60e40c05": # 増井のパソコンの場合URLも通知
     #    request = "http://connectouch.org/addlink/%s/%s?url=%s" % (readerId, nfcId, lasturl())
     #else:
-    #    request = "http://connectouch.org/addlink/%s/%s" % (readerId, nfcId)
-    request = "http://connectouch.org/addlink/%s/%s?url=%s" % (readerId, nfcId, lasturl)
+    #    request = "http://connectouch.org/addlink/%s/%s" % (readerId, nfcId)   
+    request = "http://192.168.0.200/addlink/%s/%s" % (readerIdFromIp, nfcId)
 
     try:
         res = requests.get(request)
