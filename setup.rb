43042fbc04250c5d94fee6e77ab4cd4287ea3288
Toshiyuki Masui <masui@pitecan.com>
2018-11-25 17:18:20 +0900

    ...

--- setup.rb
+++ setup.rb
@@ -1,23 +1,23 @@
-# #
-# # アプリ自動起動設定ファイルをすべてのRasPiにコピー
-# #
-# tmpfile = "/tmp/autostart"
-# (1..20).each { |n|
-#   template = <<EOF
-# @lxpanel --profile LXDE-pi
-# @pcmanfm --desktop --profile LXDE-pi
-# @xscreensaver -no-splash
-# @point-rpi
-# @chromium-browser http://192.168.0.200/index#{n}.html
-# EOF
-# 
-#   File.open(tmpfile,"w"){ |f|
-#     f.print template
-#   }
-#   cmd = "scp #{tmpfile} pi#{n}:/home/pi/.config/lxsession/LXDE-pi/"
-#   puts cmd
-#   system cmd
-# }
+#
+# アプリ自動起動設定ファイルをすべてのRasPiにコピー
+#
+tmpfile = "/tmp/autostart"
+(1..20).each { |n|
+  template = <<EOF
+@lxpanel --profile LXDE-pi
+@pcmanfm --desktop --profile LXDE-pi
+@xscreensaver -no-splash
+@point-rpi
+@chromium-browser http://192.168.0.200/index#{n}.html
+EOF
+
+  File.open(tmpfile,"w"){ |f|
+    f.print template
+  }
+  cmd = "scp #{tmpfile} pi#{n}:/home/pi/.config/lxsession/LXDE-pi/"
+  puts cmd
+  system cmd
+}
 
 #
 # public/index(1..20).html を生成
