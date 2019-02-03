e84044677d13defe69d9115269373f513f505379
saji-ryu <rsajiki@gmail.com>
2019-02-03 16:59:08 +0900

    カウントダウン機能をindex5.htmlから移植

--- Signage/Photo/src/server/routes/index.ts
+++ Signage/Photo/src/server/routes/index.ts
@@ -6,7 +6,9 @@ router.post("/", (req, res) => {
   const base64Data = req.body.img.replace(/^data:image\/png;base64,/, "");
   const n = Date.now();
   fs.writeFile("./images/" + n + ".png", base64Data, "base64", err => {
-    console.log(err);
+    if (err) {
+      console.log(err);
+    }
     res.send(`localhost:8888/images/${n}.png`);
   });
 });
